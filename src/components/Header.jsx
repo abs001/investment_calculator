01a6881d7b6af4f3125fe62b8df668baabea428e
abs001 <abhijitkumbhar001@gmail.com>
2024-06-18 05:52:59 +0530

    Added Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import logo from "../assets/investment-calculator-logo.png";
+export default function () {
+  return (
+    <header id="header">
+      <img src={logo} alt="Logo" />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+}
